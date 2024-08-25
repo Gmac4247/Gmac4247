b04ed43d1fd19542bac0893e0436e9760b175745
Gaál Sándor <gmac4247@gmail.com>
2024-08-25 15:19:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@
 setup(
     name='basic_geometry_calculator',
     version='1.0.0',
-    description=The best established and most accurate functional framework to calculate area and volume using the 3D coordinate system',
+    description='The best established and most accurate functional framework to calculate area and volume using the 3D coordinate system',
     author='Gaál Sándor',
+    copyright='® All rights reserved. Gaál Sándor 2024' ,
     author_email='gmac4247@gmail.com',
-    license=’Apache 2.0’
+    license='Apache 2.0’
     )
