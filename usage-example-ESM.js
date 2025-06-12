155979b8ee31d971beb19905d86d6391d62d7754
Gaál Sándor <gmac4247@gmail.com>
2025-06-12 14:11:44 +0200

    Create usage-example-ESM.js

---
+++ usage-example-ESM.js
@@ -0,0 +1,8 @@
+// Usage example for ES Module (import syntax)
+
+import { areaOfCircle, circumferenceOfCircle, volumeOfSphere, volumeOfCone } from './geometry.js';
+
+console.log(areaOfCircle(2));         // 12.8
+console.log(circumferenceOfCircle(2)); // 12.8
+console.log(volumeOfSphere(2));        // ≈28.84
+console.log(volumeOfCone(2, 5));       // ≈11.31
