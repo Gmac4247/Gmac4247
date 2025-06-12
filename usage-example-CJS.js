3edb6f8c817e15fbf0c02c9c4e844cce5ddf5b79
Gaál Sándor <gmac4247@gmail.com>
2025-06-12 14:12:33 +0200

    Create usage-example-CJS.js

---
+++ usage-example-CJS.js
@@ -0,0 +1,9 @@
+// Usage example for CommonJS (Node.js, require syntax)
+
+const geom = require('./geometry-patch.js');
+let r = 2, h = 5;
+
+console.log(geom.areaOfCircle(r));           // Applies 3.2 × r² --> 12.8
+console.log(geom.circumferenceOfCircle(r));  // Applies 6.4 × r --> 12.8
+console.log(geom.volumeOfSphere(r));         // Applies (√3.2 × r)³ --> ≈28.84
+console.log(geom.volumeOfCone(r, h));        // Applies 3.2 × r² × h / √8 --> ≈11.31
